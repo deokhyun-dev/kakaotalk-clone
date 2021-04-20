57e2e764fea70409b12b7104294faafbbec6f272
jcmaker0627 <jcmaker0627@gmail.com>
2021-04-20 14:09:02 +0900

    email & google login setting

--- pages/login.js
+++ pages/login.js
@@ -2,8 +2,38 @@ import styled from "styled-components";
 import Head from "next/head";
 import { Button } from "@material-ui/core";
 import { auth, provider } from "../firebase";
+import { useState } from "react";
 
 function login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await auth.createUserWithEmailAndPassword(email, password);
+      } else {
+        data = await auth.signinWithEmailAndPassword(email, password);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const toggleAccount = () => setNewAccount((prev) => !prev);
   const signIn = () => {
     auth.signInWithPopup(provider).catch(alert);
   };
@@ -15,8 +45,12 @@ function login() {
         <link rel="icon" href="/img/kakao-talk-logo.png" />
       </Head>
       <LoginContainer>
-        <Logo />
-        <EmailLogin></EmailLogin>
+        <Logo src="/img/kakao-talk-logo.png" />
+        <EmailLogin>
+          <EmailInput placeholder="Email Address" />
+          <PasswordInput placeholder="Password" />
+          <LoginButton>Login</LoginButton>
+        </EmailLogin>
         <GoogleLogin>
           <Button variant="outlined" onClick={signIn}>
             Sign in with Google
@@ -29,6 +63,37 @@ function login() {
 
 export default login;
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: #fafafa;
+`;
 
-const LoginContainer = styled.div``;
+const LoginContainer = styled.div`
+  padding: 100px;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  border: 1px solid #ffe812;
+  border-radius: 5px;
+  box-shadow: 0px 4px 10px -3px rgba(0, 0, 0, 0.7);
+`;
+
+const Logo = styled.img`
+  height: 100px;
+  width: 100px;
+  margin-bottom: 50px;
+`;
+
+const EmailLogin = styled.form``;
+
+const EmailInput = styled.input``;
+
+const PasswordInput = styled.input``;
+
+const LoginButton = styled(Button)``;
+
+const GoogleLogin = styled.div``;
